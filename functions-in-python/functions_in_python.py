68231811d7a207e991d1c5b606c0e39587dc8b2c
ashori2002 <mohammadashori2002@gmail.com>
2023-02-10 16:50:44 +0330

    The parameters we define for the function are known only inside the function and have nothing to do with external variables

--- functions-in-python/functions_in_python.py
+++ functions-in-python/functions_in_python.py
@@ -8,3 +8,10 @@ def test2(name):
     print(f"Welcome {name}")
 
 test2(input())
+####################################################################
+def test3(name):
+    print(f"Welcome {name}")
+
+name="mohammad"
+
+test3(name)
