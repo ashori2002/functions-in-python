e9c8fd08f9930f39703dbb87ba3ae89fc51a6f21
ashori2002 <mohammadashori2002@gmail.com>
2023-02-10 16:39:07 +0330

    Add project files.

---
+++ functions-in-python/functions_in_python.py
@@ -0,0 +1,10 @@
+
+def test1():
+    print("Hello world!")
+
+test1()
+###################################################################
+def test2(name):
+    print(f"Welcome {name}")
+
+test2(input())
